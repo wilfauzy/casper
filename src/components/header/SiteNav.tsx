f7b494adf05d46d131f068a9077e2b185f9c6183
Wildan Fauzy <me@wildanfauzy.com>
2021-03-10 18:49:15 +0700

    Update SiteNav.tsx

--- src/components/header/SiteNav.tsx
+++ src/components/header/SiteNav.tsx
@@ -97,7 +97,7 @@ class SiteNav extends React.Component<SiteNavProps, SiteNavState> {
                   <Link to="/about">About</Link>
                 </li>
                 <li role="menuitem">
-                  <Link to="/tags/getting-started/">Getting Started</Link>
+                  <Link to="/tags/travelling/">Travelling</Link>
                 </li>
               </ul>
               {isPost && (
